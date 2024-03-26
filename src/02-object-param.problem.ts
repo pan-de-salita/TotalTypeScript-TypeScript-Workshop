aa93136e4f3a9d0894bfc030704073cef85892a6
pan-de-salita <156948045+pan-de-salita@users.noreply.github.com>
2024-03-26 19:15:11 +0800

    completed all exercises

---
+++ src/02-object-param.problem.ts
@@ -0,0 +1,33 @@
+import { expect, it } from "vitest";
+
+interface AddTwoNumbersArgs {
+  first: number;
+  second: number;
+};
+
+export const addTwoNumbers = (params: AddTwoNumbersArgs) => {
+  // NOTE: also works:
+  // (params: {first: number; second: number})
+  // type AddTwoNumbersArgs = {
+  //   first: number;
+  //   second: number;
+  // };
+  // (params: AddTwoNumbersArgs)
+  return params.first + params.second;
+};
+
+it("Should add the two numbers together", () => {
+  expect(
+    addTwoNumbers({
+      first: 2,
+      second: 4,
+    }),
+  ).toEqual(6);
+
+  expect(
+    addTwoNumbers({
+      first: 10,
+      second: 20,
+    }),
+  ).toEqual(30);
+});
